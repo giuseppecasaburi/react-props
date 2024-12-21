08bbd7666e4c337fffc54dded6b0c1b4712ddc4d
giuseppecasaburi <enzobiondi0000@libero.it>
2024-12-21 16:32:44 +0100

    feat: milestone 3 e correzione

--- src/components/CardPost.jsx
+++ src/components/CardPost.jsx
@@ -6,7 +6,11 @@ function CardPost ({title, image, content, tags, published}) {
                 <div className="card-body">
                     <h2>{title}</h2>
                     <p>{content}</p>
-                    <p>{tags}</p>
+                    <div>
+                        {tags.map((currTag, index) => (
+                            <span key={index} className={`badge rounded-pill mx-1 p-2 ${currTag}`}>{currTag}</span>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
